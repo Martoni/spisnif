d168fc252ad60be70cb7c8902280fb3eb41efd8f
Fabien Marteau <fabien.marteau@armadeus.com>
2013-06-13 12:06:59 +0200

    [fifo_mxsx] testbench template

---
+++ hdl/fifo_mxsx.vhd
@@ -0,0 +1,39 @@
+--
+-- Copyright (c) ARMadeus Project 2013
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+--*********************************************************************
+--
+-- File          : fifo_mxsx.vhd
+-- Created on    : 13/06/2013
+-- Author        : Kevin Joly <joly.kevin25@gmail.com> &  Fabien Marteau <fabien.marteau@armadeus.com>
+--
+--*********************************************************************
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+
+Entity fifo_mxsx is
+port (
+    );
+end entity;
+
+Architecture fifo_mxsx_1 of fifo_mxsx is
+
+begin
+
+end architecture fifo_mxsx_1;
+
